d44b6007a644ec75fea9e04b4096fae4068ab350
Aasrith Chennapragada <chennapragada.aasrith@gmail.com>
2021-04-23 16:33:35 +0100

    initial

---
+++ Makefile
@@ -0,0 +1,4 @@
+
+run:
+	gcc ./src/pcap.c -o ./build/pcap -w
+	./build/pcap test-dev
---
+++ src/pcap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv){
+    if(argc<2){
+        printf("not enough arguments \n");
+    }
+    char *device;
+    device = argv[0];
+    printf("scanning %s \n");
+}
+
+
